f382287c9f7d7dd732d55ca274dbfd2cf29dba75
Luna-nera <nadabakhouche99@gmail.com>
2025-04-22 00:18:25 +0200

    Create 05. Music Wrapped.java

---
+++ 05. Music Wrapped.java
@@ -0,0 +1,21 @@
+/**
+ * Woah? What's this? 🤔 It's a weird looking comment. Another *, eh?
+ * Write a little bit about yourself here. What's your name? my name is luna
+ * Where are you from? italy.
+ *What do you do? i study informatica im new to this world!!
+ *  
+ * I'm a comment, and I want to say hi! 👋
+ *Hello!! comment
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+        System.out.println("LETS GOOSKY!!! 🔥🔥🔥🔥");
+        System.out.println("Winter bear"); // Kim Taehyung
+        System.out.println("Winter ahead"); // Kim Taehyung
+        System.out.println("Sweet night");// Kim Taehyung
+
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+    }
+}
